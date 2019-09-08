e82e28bb9dc3f611685d1b7fc2b1c0b18ca52af4
takonomura <takonomura@users.noreply.github.com>
2019-09-08 10:31:27 +0900

    Add env

--- env.sh
+++ env.sh
@@ -3,3 +3,5 @@ MYSQL_PORT=3306
 MYSQL_USER=isucari
 MYSQL_DBNAME=isucari
 MYSQL_PASS=isucari
+GOOGLE_APPLICATION_CREDENTIALS=/home/isucon/.ssh/stackdriver_trace_agen_credentials.json
+GOOGLE_CLOUD_PROJECT=isucon-213703
