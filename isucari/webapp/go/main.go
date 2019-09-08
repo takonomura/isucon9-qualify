33c440118f1f3fc63aedd41e09b0598fb07b024d
takonomura <takonomura@users.noreply.github.com>
2019-09-08 16:23:54 +0900

    Increase campaign level to 3

--- isucari/webapp/go/main.go
+++ isucari/webapp/go/main.go
@@ -495,7 +495,7 @@ func postInitialize(w http.ResponseWriter, r *http.Request) {
 
 	res := resInitialize{
 		// キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
-		Campaign: 2,
+		Campaign: 3,
 		// 実装言語を返す
 		Language: "Go",
 	}
